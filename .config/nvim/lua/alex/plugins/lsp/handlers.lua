e964bea81b3b219fd44c95c1047627706ffd4495
alex <hellport@protonmail.com>
2023-03-14 18:59:12 -0300

    disable on line diagnostics

--- .config/nvim/lua/alex/plugins/lsp/handlers.lua
+++ .config/nvim/lua/alex/plugins/lsp/handlers.lua
@@ -42,19 +42,6 @@ M.setup = function()
 		border = "rounded"
 	})
 
-	-- for use with autotags
-	vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {
-        underline = true,
-        virtual_text = {
-            spacing = 5,
-            severity_limit = 'Warning',
-        },
-        update_in_insert = true,
-    }
-)
-
 end
 
 local function highlights(client)
