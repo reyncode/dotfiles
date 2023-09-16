3b93aed6c6c8505c1533794cedc78f1c97e10292
Alex Reynolds <hellport@protonmail.com>
2023-09-16 13:41:04 -0300

    update navigation and resize

--- .config/nvim/lua/alex/core/keymaps.lua
+++ .config/nvim/lua/alex/core/keymaps.lua
@@ -27,15 +27,30 @@ vim.keymap.set ("n", "x", '"_x')
 vim.keymap.set ("v", ">", ">gv")
 vim.keymap.set ("v", "<", "<gv")
 
--- window management
+-- window creation
 vim.keymap.set ("n", "<leader>sv", "<C-w>v") 	   -- split window vertically
 vim.keymap.set ("n", "<leader>sh", "<C-w>s") 	 	 -- split window horizontally
 vim.keymap.set ("n", "<leader>sx", ":close<CR>") -- close current split window
 
+-- reminders:
+-- :new [open new window horizontally]
+-- :vne [open new window vertically]
+-- :sp {file} [open file path in window horizontally]
+-- :vs {file} [open file path in window vertically]
+-- :qa [quits all windows, saved]
+-- :!qa [quits all windows, unsaved]
+
+-- window navigation
 vim.keymap.set ("n", "<C-k>", "<C-W><C-K>") 	 -- focus top split
 vim.keymap.set ("n", "<C-j>", "<C-W><C-J>") 	 -- focus bottom split
 vim.keymap.set ("n", "<C-l>", "<C-W><C-L>") 	 -- focus right split
 vim.keymap.set ("n", "<C-h>", "<C-W><C-H>") 	 -- focus left split
+
+-- window resize
+vim.keymap.set ("n", "-", ":res -2<CR>")       -- decrease height
+vim.keymap.set ("n", "=", ":res +2<CR>")       -- increase height
+vim.keymap.set ("n", "_", ":vert res -2<CR>")  -- decrease width
+vim.keymap.set ("n", "+", ":vert res +2<CR>")  -- increase width
 
 ----------------------
 -- Plugin Keybinds
