cdf4f956716695fcc626f488356e9ecaf5f538d8
Alex Reynolds <hellport@protonmail.com>
2023-11-02 09:34:30 -0300

    toggle cmp and cleanup keymaps

--- .config/nvim/lua/alex/core/keymaps.lua
+++ .config/nvim/lua/alex/core/keymaps.lua
@@ -1,5 +1,18 @@
--- leader key
+---------------------
+-- Leader Key
+---------------------
 vim.g.mapleader = " "
+
+---------------------
+-- Reminders
+---------------------
+-- :new [open new window horizontally]
+-- :vne [open new window vertically]
+-- :sp {file} [open file path in window horizontally]
+-- :vs {file} [open file path in window vertically]
+-- :qa [quits all windows, saved]
+-- :!qa [quits all windows, unsaved]
+-- :find {file}
 
 ---------------------
 -- General Keymaps
@@ -27,20 +40,6 @@ vim.keymap.set ("n", "x", '"_x')
 vim.keymap.set ("v", ">", ">gv")
 vim.keymap.set ("v", "<", "<gv")
 
--- window creation
-vim.keymap.set ("n", "<leader>sv", "<C-w>v") 	   -- split window vertically
-vim.keymap.set ("n", "<leader>sh", "<C-w>s") 	 	 -- split window horizontally
-vim.keymap.set ("n", "<leader>sx", ":close<CR>") -- close current split window
-
--- reminders:
--- :new [open new window horizontally]
--- :vne [open new window vertically]
--- :sp {file} [open file path in window horizontally]
--- :vs {file} [open file path in window vertically]
--- :qa [quits all windows, saved]
--- :!qa [quits all windows, unsaved]
--- :find {file}
-
 -- window navigation
 vim.keymap.set ("n", "<C-k>", "<C-W><C-K>") 	 -- focus top split
 vim.keymap.set ("n", "<C-j>", "<C-W><C-J>") 	 -- focus bottom split
@@ -61,9 +60,7 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 -- buffer
--- vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
--- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_actions)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "gh", vim.lsp.buf.hover)
@@ -77,7 +74,9 @@ vim.keymap.set("n", "gr", vim.lsp.buf.references)
 -- nvim-tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")  -- toggle file explorer
 vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>")   -- focus file explorer
--- vim.keymap.set("n", "<leader>r", ":NvimTreeRefresh<CR>") -- refresh file explorer 
 
 -- telescope
 vim.keymap.set("n", "<leader>t", ":Telescope<CR>")  -- toggle file explorer
+
+--nvim-cmp
+vim.keymap.set("n", "<leader>c", "<cmd>lua vim.g.cmp_toggle = not vim.g.cmp_toggle<CR>") -- toggle completions
--- .config/nvim/lua/alex/plugins/cmp.lua
+++ .config/nvim/lua/alex/plugins/cmp.lua
@@ -1,34 +1,33 @@
 local cmp = require("cmp")
 
+vim.g.cmp_toggle = true
+
 cmp.setup({
   snippet = {
     expand = function(args)
-        vim.fn['vsnip#anonymous'](args.body)
+      vim.fn['vsnip#anonymous'](args.body)
     end
   },
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({
-       behavior = cmp.ConfirmBehavior.Replace,
-       select = true,
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     }),
     ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
     ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-   },
-   sources = {
-      { name = "nvim_lsp" },
-      { name = "vsnip" },
-      { name = "path" },
-      { name = "buffer" },
-   },
-    window = {
-      completion = cmp.config.window.bordered(),
-    },
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+  },
+  enabled = function()
+      return vim.g.cmp_toggle
+  end,
+
 })
 
 vim.cmd([[
